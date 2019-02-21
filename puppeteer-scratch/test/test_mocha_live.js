a9da13f1a8fcb477d6b47c8573c5f5ac820dd7cb
Urian <uchang@sightmachine.com>
2019-02-20 23:48:13 -0800

    Added before and beforeEach methods to test suites to see how they are executed. Enabled skip, so the test isn't usually run

--- puppeteer-scratch/test/test_mocha_live.js
+++ puppeteer-scratch/test/test_mocha_live.js
@@ -1,7 +1,38 @@
 var assert = require('assert');
 
-describe('Array', function() {
+describe.skip('Array', function() {
+  before(function() {
+    console.log("BEFORE ALL - 1");
+  });
+  beforeEach(function() {
+    console.log("BEFORE EACH - 1");
+  });
+
   describe('#indexOf()', function() {
+    before(function() {
+      console.log("BEFORE ALL - 1b");
+    });
+    beforeEach(function() {
+      console.log("BEFORE EACH - 1b");
+    });
+
+    it('should return -1 when the value is not present', function() {
+      assert.equal([1, 2, 3].indexOf(4), -1);
+    });
+  });
+
+  describe('#indexOf() - copy', function() {
+    before(function() {
+      console.log("BEFORE ALL - 1c");
+    });
+    beforeEach(function() {
+      console.log("BEFORE EACH - 1c");
+    });
+
+    it('should return -1 when the value is not present', function() {
+      assert.equal([1, 2, 3].indexOf(4), -1);
+    });
+
     it('should return -1 when the value is not present', function() {
       assert.equal([1, 2, 3].indexOf(4), -1);
     });
