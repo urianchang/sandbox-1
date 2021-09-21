1505db1cdbac9fa0c5b69234eead87a80c291900
Urian Chang <urian.chang@dominodatalab.com>
2021-09-21 14:23:18 -0700

    Migrated work into 'sandbox' repo

--- LearnTS/README.md
+++
@@ -1,2 +0,0 @@
-# TypeScript Practice
-Tutorials and exercises completed while learning how to use TypeScript.
--- LearnTS/ts-express/src/server.ts
+++
@@ -1,9 +0,0 @@
-// lib/server.ts
-
-import app from "./app";
-const PORT = 3000;
-
-app.listen(PORT, () => {
-  console.log('Express server listening on port ' + PORT);
-});
-
